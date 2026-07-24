2c69cabc8efeec1f34f1edda36ba57fb7146c450
0Maheswara <113922406+0Maheswara@users.noreply.github.com>
2026-07-24 20:50:43 +0530

    Add application entry point with DOMContentLoaded listener

--- js/core/app.js
+++ js/core/app.js
@@ -1,1 +1,11 @@
+/* ==========================================================
+   APPLICATION ENTRY POINT
+========================================================== */
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Application Started");
+
+    Router.init();
+
+});
